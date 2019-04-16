9844b5f8716c251e61478ed94bee7cb83bfcd48b
Amy Cen <amy.cen@blackrock.com>
2019-04-16 03:00:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
  # your code here!
+ res = []
+ if collection.empty?
+   "This block should not run!"
+ else
+   i = 0
+   while i < collection.length
+     yield (collection[i]) == 
 end
